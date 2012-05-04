0aa7ccb03d6ea9bd522d8ef04b9a1a7742dcdca9
Dan Buch <d.buch@modcloth.com>
2012-05-04 13:53:28 -0400

    Using the existing rspec `subject` definition

--- spec/mc_settings_spec.rb
+++ spec/mc_settings_spec.rb
@@ -5,57 +5,57 @@ describe Setting do
   context "Test with stubs" do
     before :each do
       stub_setting_files
-      Setting.reload(
+      subject.reload(
           :path  => "config/settings",
           :files => ["default.yml", "environments/test.yml"],
           :local => true)
     end
 
     it 'should return test specific values' do
-      Setting.available_settings['one'].should == "test"
-      Setting.one.should == "test"
-      Setting['one'].should == "test"
+      subject.available_settings['one'].should == "test"
+      subject.one.should == "test"
+      subject['one'].should == "test"
     end
 
     it "should handle custom values overriding everything else" do
-      Setting.seven.should == "seven from custom"
+      subject.seven.should == "seven from custom"
     end
 
     it "handles multiple values" do
-      Setting[:six].should == {"default"=>"default value", "extra"=>"recursively overriden", "deep_level"=>{"value"=>"even deeper level"}}
-      Setting.available_settings['six']['default'].should == "default value"
-      Setting.seven.should == "seven from custom"
+      subject[:six].should == {"default"=>"default value", "extra"=>"recursively overriden", "deep_level"=>{"value"=>"even deeper level"}}
+      subject.available_settings['six']['default'].should == "default value"
+      subject.seven.should == "seven from custom"
     end
 
     it "handles default key" do
-      Setting.default_setting.should == 1
-      Setting['seven']['default'].should == "seven from custom"
+      subject.default_setting.should == 1
+      subject['seven']['default'].should == "seven from custom"
     end
 
     it "should handle empty strings" do
-      Setting.empty.should == ""
+      subject.empty.should == ""
     end
 
     it "should responds to ? mark" do
-      Setting.autologin?.should == true
+      subject.autologin?.should == true
     end
 
     it "should returns false correctly" do
-      Setting.flag_false.should be(false)
+      subject.flag_false.should be(false)
     end
 
     it "should merge keys recursivelly" do
-      Setting.six(:extra).should == "recursively overriden"
-      Setting.six(:deep_level, :value).should == "even deeper level"
+      subject.six(:extra).should == "recursively overriden"
+      subject.six(:deep_level, :value).should == "even deeper level"
     end
 
     it "should create keys if it does not exist" do
-      Setting.test_specific.should == "exist"
+      subject.test_specific.should == "exist"
     end
 
     context "working with arrays" do
       it "should replace the whole array instead of appending new values" do
-        Setting.nested_array.should == ['first', 'four', 'five']
+        subject.nested_array.should == ['first', 'four', 'five']
       end
     end
   end
@@ -64,7 +64,7 @@ describe Setting do
     it "should keep its values" do
       3.times do |time|
         Thread.new {
-          Setting.available_settings.shoud_not be_empty
+          subject.available_settings.shoud_not be_empty
         }
       end
     end
@@ -72,89 +72,89 @@ describe Setting do
 
   context "Test from file" do
     before :each do
-      Setting.reload(
+      subject.reload(
          :path  => File.join(File.dirname(__FILE__)) + '/fixtures',
          :files => ['sample.yml']
       )
     end
 
     it 'should support [] syntax' do
-      Setting['tax']['default'].should == 0.0
-      Setting['tax'].should == { 'default' => 0.0, 'california' => 7.5 }
+      subject['tax']['default'].should == 0.0
+      subject['tax'].should == { 'default' => 0.0, 'california' => 7.5 }
     end
 
     it 'should support method invocation syntax' do
-      Setting.tax.should == 0.0
+      subject.tax.should == 0.0
 
-      Setting.tax(:default).should     == Setting.tax
-      Setting.tax('default').should    == Setting.tax
-      Setting.tax(:california).should  == 7.5
+      subject.tax(:default).should     == subject.tax
+      subject.tax('default').should    == subject.tax
+      subject.tax(:california).should  == 7.5
 
-      Setting.states.should            == ['CA', 'WA', 'NY']
-      Setting.states(:default).should  == Setting.states
-      Setting.states(:ship_to).should  == ['CA', 'NY']
+      subject.states.should            == ['CA', 'WA', 'NY']
+      subject.states(:default).should  == subject.states
+      subject.states(:ship_to).should  == ['CA', 'NY']
     end
 
     it 'should correctly process Boolean values' do
-      Setting.boolean_true?.should be(true)
-      Setting.boolean_true.should == 4
-      Setting.boolean_false?.should be(false)
-      Setting.boolean_false?(:default).should be(false)
-      Setting.boolean_false?(:negated).should be(true)
+      subject.boolean_true?.should be(true)
+      subject.boolean_true.should == 4
+      subject.boolean_false?.should be(false)
+      subject.boolean_false?(:default).should be(false)
+      subject.boolean_false?(:negated).should be(true)
     end
   end
 
   context "Test recursive overrides and nested hashes" do
     before :each do
-      Setting.reload(
+      subject.reload(
          :path  => File.join(File.dirname(__FILE__)) + '/fixtures',
          :files => ['sample.yml', 'joes-colors.yml']
       )
     end
 
     it 'should override colors with Joes and support nested hashes' do
-      Setting.color.should == :grey # default
-      Setting.color(:pants).should == :purple # default
+      subject.color.should == :grey # default
+      subject.color(:pants).should == :purple # default
 
-      Setting.color(:pants, :school).should == :blue # in sample
-      Setting.color(:pants, :favorite).should == :orange # joes override
+      subject.color(:pants, :school).should == :blue # in sample
+      subject.color(:pants, :favorite).should == :orange # joes override
 
-      Setting.color(:shorts, :school).should == :black # in sample
-      Setting.color(:shorts, :favorite).should == :white # joe's override
+      subject.color(:shorts, :school).should == :black # in sample
+      subject.color(:shorts, :favorite).should == :white # joe's override
 
-      Setting.color(:shorts).should == :stripes # joe's override of default
+      subject.color(:shorts).should == :stripes # joe's override of default
     end
 
   end
   context "Complex nested configs" do
     before :each do
-      Setting.reload(
+      subject.reload(
           :path  => File.join(File.dirname(__FILE__)) + '/fixtures',
           :files => ['shipping.yml']
       )
     end
     it "should build correct tree with arrays and default values " do
-      Setting.shipping_config.should == "Defaulted"
-      Setting.shipping_config(:domestic, :non_shippable_regions).first.should == "US-AS"
-      Setting.shipping_config(:international, :service).should == 'Foo'
-      Setting.shipping_config(:international, :countries).size.should > 0
-      Setting.shipping_config(:international, :shipping_carrier).should == 'Bar'
+      subject.shipping_config.should == "Defaulted"
+      subject.shipping_config(:domestic, :non_shippable_regions).first.should == "US-AS"
+      subject.shipping_config(:international, :service).should == 'Foo'
+      subject.shipping_config(:international, :countries).size.should > 0
+      subject.shipping_config(:international, :shipping_carrier).should == 'Bar'
       #backward compatibility:
-      Setting.shipping_config(:domestic)['non_shippable_regions'].size.should > 0
+      subject.shipping_config(:domestic)['non_shippable_regions'].size.should > 0
     end
   end
 
   context "Ruby code inside yml file" do
     before :each do
-      Setting.reload(
+      subject.reload(
           :path  => File.join(File.dirname(__FILE__)) + '/fixtures',
           :files => ['shipping.yml']
       )
     end
     it "should interpret ruby code and put correct values" do
-      Setting.shipping_config.should == "Defaulted"
-      Setting.number == 5
-      Setting.stringified == "stringified"
+      subject.shipping_config.should == "Defaulted"
+      subject.number == 5
+      subject.stringified == "stringified"
     end
   end
 end
